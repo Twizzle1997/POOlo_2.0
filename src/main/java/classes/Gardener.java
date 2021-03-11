3953373b09dbe17c21a0f495b15733f318c031d8
Audrey <audrey.cornaire@gmail.com>
2021-03-11 09:13:39 +0100

    Update Gardener, add dependencies and add GardenerTest

--- src/main/java/classes/Gardener.java
+++ src/main/java/classes/Gardener.java
@@ -1,4 +1,5 @@
 package classes;
+
 import classes.Vegetables.*;
 
 public class Gardener {
@@ -52,27 +53,21 @@ public class Gardener {
             vegetable.grow(0);
             vegetable.set_type("pickle");
             return vegetable;
-        }
-
-        else {
+        } else {
             throw new Exception("Oups ! Nous n'avons plus de légume de type " + unType + " en magasin...");
         }
     }
 
     public void add(Garden cls_garden, Vegetable obj) throws Exception {
 
-        if (cls_garden.get_seed() + obj.get_seed() < cls_garden.get_maxCapacity())
-        {
-            if (cls_garden.get_vegetables().containsKey(obj.get_type()))
-            {
+        if (cls_garden.get_seed() + obj.get_seed() < cls_garden.get_maxCapacity()) {
+            if (cls_garden.get_vegetables().containsKey(obj.get_type())) {
                 cls_garden.set_vegetables(obj.get_type(), cls_garden.get_vegetables().get(obj.get_type()) + obj.get_seed());
-                cls_garden.set_seed(cls_garden.get_seed() + obj.get_seed());
-            }else
-            {
+            } else {
                 cls_garden.set_vegetables(obj.get_type(), obj.get_seed());
-                cls_garden.set_seed(cls_garden.get_seed() + obj.get_seed());
             }
-        }else {
+            cls_garden.set_seed(cls_garden.get_seed() + obj.get_seed());
+        } else {
             throw new Exception("Jardin surchargé ! La capacité maximum de ce jardin est de " + cls_garden.get_maxCapacity() + " graines !");
         }
     }
---
+++ src/test/java/classes/GardenerTest.java
@@ -0,0 +1,19 @@
+package classes;
+
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class GardenerTest {
+
+    @Test
+    public void testPlant_vegetable() {
+    }
+
+    @Test
+    public void testTestPlant_vegetable() {
+    }
+
+    @Test
+    public void testAdd() {
+    }
+}
